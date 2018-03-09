7148385e7fc93a19d74ad553c1921241e8d0fbd8
Romain Marquet <romain.marquet@hotmail.fr>
2018-03-09 11:11:28 +0100

    heroku

--- routes/index.js
+++ routes/index.js
@@ -37,15 +37,17 @@ router.get('/friends', function (req, res, next) {
   res.json(data);
 });
 
+router.use(fileUpload());
+
 router.post('/upload', function(req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let uploadedAvatar = req.files.avatar;
+  let profilePicUp = req.files.profilePicUp;
 
   // Use the mv() method to place the file somewhere on your server
-  uploadedAvatar.mv('./public/images/'+'avatar'+'.png', function(err) {
+  profilePicUp.mv('./public/images/'+'avatar'+'.jpg', function(err) {
     if (err)
       return res.status(500).send(err);
 
